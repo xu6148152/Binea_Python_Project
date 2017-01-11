0180fdebd317072a2571b2ead58aba28d1892f01
Binea <xu6148152@gmail.com>
2017-01-11 22:36:38 +0800

    Add DataStructure

--- PythonCookbook/datastruct_algorithm/datastruct.py
+++ PythonCookbook/datastruct_algorithm/datastruct.py
@@ -4,6 +4,8 @@ import heapq
 import json
 
 '''测试占位符* _'''
+
+
 def test_placeholder():
     data = ['ACME', 50, 91.1, (2012, 12, 21)]
     name, shares, price, date = data
@@ -16,7 +18,10 @@ def test_placeholder():
     name, email, *phone_numbers = record
     print(name, email, *phone_numbers)
 
+
 '''利用deque保存最近一定数量的数据'''
+
+
 def search(lines, pattern, history=5):
     previous_lines = deque(maxlen=history)
     for li in lines:
@@ -41,7 +46,10 @@ def test_search():
             print(line, end='')
             print('-' * 20)
 
+
 '''测试hea'''
+
+
 def test_heapq():
     nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
     print(get_n_largest(nums, 1))
@@ -55,7 +63,10 @@ def test_heapq():
     ]
     print(heapq.nlargest(3, portfolio, key=lambda s: s['price']))
 
+
 '''利用heapq构造优先队列'''
+
+
 class PriorityQueue:
     def __init__(self):
         self._queue = []
@@ -89,6 +100,7 @@ def test_priorityqueue():
     print(q.pop())
     print(q.pop())
 
+
 def testDict():
     d = defaultdict(list)
     d['a'].append(1)
@@ -113,5 +125,193 @@ def testDict():
 
     print(json.dumps(d))
 
+
+def test_dict_compute():
+    prices = {
+        'ACME': 45.23,
+        'AAPL': 612.78,
+        'IBM': 205.55,
+        'HPQ': 37.20,
+        'FB': 10.75
+    }
+
+    # zip的结果只能访问一次
+    min_price = min(zip(prices.values(), prices.keys()))
+    print(min_price)
+    max_price = max(zip(prices.values(), prices.keys()))
+    print(max_price)
+
+    prices_sorted = sorted(zip(prices.values(), prices.keys()))
+    print(prices_sorted)
+
+    print(min(prices, key=lambda k: prices[k]))
+    print(max(prices, key=lambda k: prices[k]))
+
+
+def test_dict_same():
+    a = {
+        'x': 1,
+        'y': 2,
+        'z': 3
+    }
+
+    b = {
+        'w': 10,
+        'x': 11,
+        'y': 2,
+    }
+
+    print(a.keys() & b.keys())
+    print(a.keys() - b.keys())
+    print(a.items() & b.items())
+
+
+def dedupe(items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+
+def test_dedupe():
+    a = [1, 5, 2, 1, 9, 1, 5, 10]
+    print(list(dedupe(a)))
+
+def test_slice():
+    items = [0, 1, 2, 3, 4, 5, 6]
+    a = slice(2, 4)
+    print(items[a])
+    items[a] = [10, 11]
+    print(items)
+    del items[a]
+    print(items)
+
+
+def test_counter():
+    words = [
+        'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+        'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+    ]
+
+    from collections import Counter
+    word_counts = Counter(words)
+    top_three = word_counts.most_common(3)
+    print(top_three)
+
+
+def test_itemgetter():
+    rows = [
+        {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
+        {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
+        {'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
+        {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
+    ]
+
+    from operator import itemgetter
+
+    rows_by_fname = sorted(rows, key=itemgetter('fname'))
+    print(rows_by_fname)
+    rows_by_uid = sorted(rows, key=itemgetter('uid'))
+    print(rows_by_uid)
+
+
+def test_groupby():
+    rows = [
+        {'address': '5412 N CLARK', 'date': '07/01/2012'},
+        {'address': '5148 N CLARK', 'date': '07/04/2012'},
+        {'address': '5800 E 58TH', 'date': '07/02/2012'},
+        {'address': '2122 N CLARK', 'date': '07/03/2012'},
+        {'address': '1060 W ADDISON', 'date': '07/02/2012'}
+    ]
+
+    from operator import itemgetter
+    from itertools import groupby
+
+    rows.sort(key=itemgetter('date'))
+    for date, items in groupby(rows, key=itemgetter('date')):
+        print(date)
+        for i in items:
+            print(' ', i)
+
+def test_filter():
+    mylist = [1, 4, -5, 10, -7, 2, 3, -1]
+    # generator
+    pos = (n for n in mylist if n > 0)
+    for x in pos:
+        print(x)
+
+    ivals = list(filter(is_int, mylist))
+    print(ivals)
+
+
+def is_int(val):
+    try:
+        x = int(val)
+        return x > 0
+    except ValueError:
+        return False
+
+
+def test_compress():
+    rows = [
+        {'address': '5412 N CLARK', 'date': '07/01/2012'},
+        {'address': '5148 N CLARK', 'date': '07/04/2012'},
+        {'address': '5800 E 58TH', 'date': '07/02/2012'},
+        {'address': '2122 N CLARK', 'date': '07/03/2012'},
+        {'address': '1060 W ADDISON', 'date': '07/02/2012'}
+    ]
+
+    counts = [0, 3, 10, 4, 1, 7, 6, 1]
+
+    from itertools import compress
+    more5 = [n > 5 for n in counts]
+    print(more5)
+    print(list(compress(rows, more5)))
+
+
+def test_subdict():
+    prices = {
+        'ACME': 45.23,
+        'AAPL': 612.78,
+        'IBM': 205.55,
+        'HPQ': 37.20,
+        'FB': 10.75
+    }
+
+    p1 = {key: value for key, value in prices.items() if value > 200}
+    print(p1)
+
+
+def test_namedtuple():
+    from collections import namedtuple
+    Subscriber = namedtuple('Subscriber', ['addr', 'joined'])
+    sub = Subscriber('jonesy@example.com', '2012-10-19')
+    print(sub)
+    print(sub.addr)
+    print(sub.joined)
+
+
+def dict_to_stock(s):
+    from collections import namedtuple
+    Stock = namedtuple('Stock', ['name', 'shares', 'price', 'date', 'time'])
+    stock_prototype = Stock('', 0, 0.0, None, None)
+    stock_prototype._replace(**s)
+
+
+def test_dict_to_namedtuple():
+    a = {'name': 'ACME', 'shares': 100, 'price': 123.45}
+    print(dict_to_stock(a))
+
+
+def test_chain_map():
+    a = {'x': 1, 'z': 3}
+    b = {'y': 2, 'z': 4}
+
+    from collections import ChainMap
+    c = ChainMap(a, b)
+    print(c['z'])
+
+
 if __name__ == '__main__':
-    testDict()
+    test_chain_map()
+
